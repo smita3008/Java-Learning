8b619e2c6dc3881db1c9644dd7271694928935db
smita3008 <smitamane94@gmail.com>
2025-03-07 15:39:08 +0530

    naming convention

---
+++ Name.java
@@ -0,0 +1,13 @@
+
+
+public class Name {
+    public static void main(String[] args) {
+        char s='\u0938';
+        char s1 = '\u094D';
+        char m='\u092E';
+        char m1 ='\u093F';
+        char t = '\u0924';
+        char t1 = '\u093E';
+        System.out.println("My name is="+s+s1+m+m1+t+t1);
+    }
+}
