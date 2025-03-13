b15018a1134eef86610d02d3a57c14d8ee9625b5
smita3008 <smitamane94@gmail.com>
2025-03-13 16:12:39 +0530

    To calculate Square,Cube,Forth of user input number

--- PowerOfNumber.java
+++ PowerOfNumber.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class PowerOfNumber {
+public class PowerOfNumber
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+           System.out.println("Enter the number:" );
+        double num = sc.nextDouble();
+
+        double square = Math.pow(num,2);
+        double cube = Math.pow(num,3);
+        double fourth = Math.pow(num,4);
+
+
+        System.out.println("Square of the number=" + square);
+        System.out.println("Cube of the number"+cube);
+        System.out.println("Fourth of the number" +fourth);
+
+    }
 }
