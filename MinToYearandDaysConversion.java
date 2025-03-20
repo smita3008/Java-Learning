a995a1f24bb970402065925a5ee443e2e7bc8c1d
smita3008 <smitamane94@gmail.com>
2025-03-20 17:36:14 +0530

    Minutes to Year and Days conversion

--- MinToYearandDaysConversion.java
+++ MinToYearandDaysConversion.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class MinToYearandDaysConversion {
+public class MinToYearandDaysConversion
+{
+    public static void main(String[] args) {
+
+        int minutesInYear=60*24*365;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter minutes=");
+        double minutes= sc.nextDouble();
+        int year = (int) (minutes)/(minutesInYear);
+        int days = (int) (minutes/60/24) % 365;
+                        System.out.println("years=" + year + "\nDays="+days);
+    }
 }
