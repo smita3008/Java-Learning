cec96212fafde44c5444a1a290279c2ef15950f4
smita3008 <smitamane94@gmail.com>
2025-03-20 18:16:36 +0530

    Print greater number among 3 user input numbers

---
+++ GreaterNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class GreaterNumber
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter three numbers");
+        int num1= sc.nextInt();
+        int num2= sc.nextInt();
+        int num3= sc.nextInt();
+        int temp = ((num1 > num2) ? num1 : num2);
+        int max = ((num3 >temp) ? num3:temp);
+        System.out.println("Greater number is=" + max);
+
+
+
+    }
+}
