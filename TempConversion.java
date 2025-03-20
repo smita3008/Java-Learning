686db065c8fbe9097275f706542ffac0be8428f5
smita3008 <smitamane94@gmail.com>
2025-03-20 17:48:18 +0530

    Convert Celsius to Fahrenheit and Kelvin

---
+++ TempConversion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TempConversion
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Celsius Temp");
+        double celsius = sc.nextDouble();
+        double Fahrenheit= celsius*(1.8)+(32); // 1 far
+        double Kelvin = celsius*(1.8)+273;
+        System.out.println("Conversion of Celsius to Fahrenheit is " + Fahrenheit);
+        System.out.println("Conversion of Celsius to Kelvin is"+Kelvin);
+
+    }
+}
