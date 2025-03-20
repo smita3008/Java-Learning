0050f5cd98aadd066fbecc43f8f04f55360c3fa0
smita3008 <smitamane94@gmail.com>
2025-03-20 18:25:01 +0530

    Greater number among 3 numbers

--- GreaterNum.java
+++ GreaterNum.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
-public class GreaterNumber
+public class GreaterNum
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter three numbers");
         int num1= sc.nextInt();
