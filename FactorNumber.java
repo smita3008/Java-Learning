a65d6dbd01c588262185e1953e1b5c672f50f0f0
smita3008 <smitamane94@gmail.com>
2025-03-21 17:51:14 +0530

    Add files via upload

---
+++ FactorNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FactorNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num = sc.nextInt();
+        for (int i=1;i<=num;i++)
+        {
+            if(num %i==0)
+            {
+                System.out.println("Factor of number =" + i);
+            }
+        }
+    }
+}
