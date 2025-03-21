fb738efd503dd52abe8fd7bc4dce59b6e2010b50
smita3008 <smitamane94@gmail.com>
2025-03-21 17:49:12 +0530

    fact

--- FactorNumber.java
+++ FactorNumber.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class FactorNumber {
+public class FactorNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num = sc.nextInt();
+        for (int i=1;i<=num;i++)
+        {
+            if(num %i==0)
+            {
+                System.out.println("Factor of number =" + i);
+            }
+        }
+    }
 }
