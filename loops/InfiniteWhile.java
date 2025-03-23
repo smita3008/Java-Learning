22826c06c3bd87a5ce712e1666ab37b3eb335301
smita3008 <smitamane94@gmail.com>
2025-03-23 14:30:06 +0530

    Infinite loop

---
+++ loops/InfiniteWhile.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class InfiniteWhile
+{
+    public static void main(String[] args) {
+        int i=0;
+        while (i<1)
+        {
+            System.out.println(i);
+        }
+    }
+}
