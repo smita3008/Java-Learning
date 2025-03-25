3f4f2c81fbbcf4b5f85da655243355880d347489
smita3008 <smitamane94@gmail.com>
2025-03-25 21:21:47 +0530

    Add files via upload

--- InfiniteFor.java
+++ InfiniteFor.java
@@ -1,12 +1,12 @@
 package loops;
 
-public class InfiniteWhile
+public class InfiniteFor
 {
     public static void main(String[] args) {
-        int i=1;
-        while (i<=1)
+        for(int i=1;i<=5;i++)
         {
             System.out.println(i);
+            i=i-1;
         }
     }
 }
